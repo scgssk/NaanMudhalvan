a2d816621c16a78e0731fbc687b26d5653f50df9
Heisenberg794 <scgsoorya794@gmail.com>
2025-11-14 05:59:40 +0530

    cors

--- src/main/java/com/simportal/config/WebConfig.java
+++ src/main/java/com/simportal/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://naanmudhalvan-sim.vercel.app")
                 .allowedMethods("GET","POST","PUT","DELETE");
     }
 }
